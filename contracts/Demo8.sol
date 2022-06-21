e256e34c48ad294bd026da13358f3cece06dc690
"tobala" <"ibalasite@gmail.com">
2022-06-21 12:02:35 +0800

    add demo8

---
+++ contracts/Demo8.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.4;
+
+contract Demo8 {
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+    modifier costs(uint256 price){
+        if(msg.value >= price) {
+            _;
+        }
+    }
+
+
+}
+contract Register is Demo8{
+    mapping(address=> bool) public registeredAddresses;
+    uint public price;
+    constructor(uint256 initialPrice){
+        price= initialPrice;
+    }
+    function register() public payable costs(price){
+        registeredAddresses[msg.sender]=true;
+    }
+    function changePrice(uint256 _price) public onlyOwner{
+        price=_price;
+    }
+}
