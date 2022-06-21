df3ce8ffd3dad3907653e4a65c99e98c6949e2dd
"tobala" <"ibalasite@gmail.com">
2022-06-21 11:35:56 +0800

    modify demo6

--- contracts/Demo6.sol
+++ contracts/Demo6.sol
@@ -3,14 +3,21 @@ pragma solidity 0.7.4;
 
 contract Demo6 {
     mapping(address=>uint256) public balances;
+    constructor(){
+        balances[address(this)]=10000;
+    }
     function add100(address acc) public{
          balances[acc]+=100;
+         balances[address(this)]-= 100;
     }
     function getWelcome() public {
         balances[msg.sender]+= 50;
     }
     function updateBalance(uint256 newBalance) public{
         balances[msg.sender]=newBalance;
+    }
+    function getExternalOwnedAccountBalance() public view returns(uint256) {
+        return balances[msg.sender];
     }
     function getSelfBalance() public view returns(uint256){
 
