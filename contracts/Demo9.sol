42dea955c0ae971d85d66f18a00c106334fb1c32
"tobala" <"ibalasite@gmail.com">
2022-06-21 14:28:19 +0800

    add demo9

---
+++ contracts/Demo9.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.4;
+pragma experimental ABIEncoderV2;
+contract Demo9 {
+
+    string[] public myArray;
+    constructor(){
+        myArray.push("hi");
+    }
+    function getArray() public view returns(string[] memory){
+        return myArray;
+    }
+}
