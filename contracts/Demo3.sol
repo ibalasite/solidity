df8d0fda7ab6403ea3e093096a2a9b916daaf345
"tobala" <"ibalasite@gmail.com">
2022-06-21 09:47:20 +0800

    modify demo2 number 66899

--- contracts/Demo3.sol
+++ contracts/Demo3.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.7.4;
-contract Demo3{
+pragma solidity 0.7.4;
+contract Demo3 {
     function test1GetLength() public pure returns(uint256){
         uint[10] memory bid;
         return bid.length;
@@ -9,10 +8,22 @@ contract Demo3{
         uint[] memory bid = new uint[](20);
         return bid.length;
     }
-     function test3GetFirstElement() public pure returns(uint256){
+    function test3GetFirstElement() public pure returns(uint256){
         uint[10] memory bid;
         bid[0]=50;
         bid[1]=100;
         return bid[0];
     }
+    function test4GetWrongElement() public pure returns(uint256) {
+        uint[] memory bid = new uint[](10);
+        return bid[10];
+    }
+    
+    function test5GetWrongElement() public pure returns(uint256) {
+        uint[10] memory bid;
+        // will be warned in compile time
+        //return bid[10];
+        return bid[9];
+    }
+
 }
