f605a03c82085c51de9bbff7812d05c3f8a6e7f3
"tobala" <"ibalasite@gmail.com">
2022-06-23 16:52:38 +0800

    change version test

--- contracts/CAC.sol
+++ contracts/CAC.sol
@@ -1,40 +1,50 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
 contract CAC{
     struct SpendingRequest{
         string description;
         uint256 value;
         address vendorAccount;
         bool complete;
+        uint approvalCount;
+        mapping(address=>bool) approvedUsers;
     }
-    SpendingRequest[] public requests;
+    uint requestCount;
+    mapping(uint=>SpendingRequest) public requests;
+    //SpendingRequest[] public requests;
     modifier restricted(){
         require(msg.sender==manager);
         _;
     }
     address public manager;
     uint256 public minimumFund;
-    address[] public approvers;
-    constructor(uint256 fund) public{
+    mapping(address => bool) public approvers;
+    constructor(uint256 fund) {
         manager = msg.sender;
         minimumFund= fund;
     }
     function join() public payable{
         require(msg.value > minimumFund);
-        approvers.push(msg.sender);
-
+        approvers[msg.sender] = true;
     }
     function createRequest(
         string memory des,
         uint256 value,
         address vendor
     ) public restricted {
-        SpendingRequest memory req = SpendingRequest({
-            description: des,
-            value: value,
-            vendorAccount: vendor,
-            complete: false
-        });
-        requests.push(req);
+        SpendingRequest storage req = requests[requestCount++];
+        req.description = des;
+        req.value =value;
+        req.vendorAccount= vendor;
+        req.complete =false;
+        req.approvalCount = 0;
+    }
+    function approvalRequest(uint256 index) public {
+        SpendingRequest storage request= requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvedUsers[msg.sender]);
+        request.approvalCount++;
+        request.approvedUsers[msg.sender]=true;
     }
 
 }
